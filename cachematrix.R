99cdc07e606e40eafee04e41dbfa68c22a380266
ddpandey <ddpandey@gmail.com>
2015-02-22 14:40:45 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,10 @@
 
 ## Generates a cached matrix based on the inputs
+## The following function calculates the mean of the special "matrix" created with the above function. 
+## However, it first checks to see if the mean has already been calculated. 
+## If so, it gets the mean from the cache and skips the computation. 
+## Otherwise, it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
+
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -15,11 +20,16 @@ makeCacheMatrix <- function(x = matrix()) {
        getmean = getmean)
 }
 
+## Invoking the solve() function to return the inverse
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve 
+## should retrieve the inverse from the cache.
+## 
+
 cacheSolve <- function(x, ...) {
 
-## Invoking the solve() function to return the inverse
-
-        m <- x$solve()
+## Computing the inverse of a square matrix can be done with the solve function in R.
+      m <- x$solve()
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
